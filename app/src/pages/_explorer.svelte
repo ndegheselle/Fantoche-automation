c5ff5de73d688da7ba4bd78f0dde1f132ef38a8e
Nicolas de gheselle <nicolas@degheselle.fr>
2023-12-12 21:33:49 +0100

    Base docker setup

---
+++ app/src/pages/_explorer.svelte
@@ -0,0 +1,15 @@
+<script>
+    let { scopes = [] } = $props();
+    let selectedScope = scopes[0];
+</script>
+
+<div class="list-group">
+    {#each scopes as scope}
+        <a
+            href="#"
+            class="list-group-item list-group-item-action"
+            class:active={scope.id == selectedScope.id}
+            on:click={() => selectedScope = scope}>{scope.name}</a
+        >
+    {/each}
+</div>
