6162082da85e302cd16ac5c8818f415523e083a7
NicolasDeGheselle <nicolas@degheselle.fr>
2025-01-24 16:59:12 +0100

    Add nodify gestures

---
+++ Automation/Automation.App/Views/WorkPages/Workflows/Editor/Gestures/DefautGestureMappings.cs
@@ -0,0 +1,16 @@
+﻿using Nodify;
+using Nodify.Interactivity;
+using System.Windows.Input;
+
+namespace Automation.App.Views.WorkPages.Workflows.Editor.Gestures
+{
+    public class DefautGestureMappings : EditorGestures
+    {
+        public static readonly DefautGestureMappings Instance = new DefautGestureMappings();
+
+        private DefautGestureMappings()
+        {
+            Editor.ZoomModifierKey = ModifierKeys.Control;
+        }
+    }
+}
---
+++ Automation/Automation.App/Views/WorkPages/Workflows/Editor/Gestures/LockedGestureMappings.cs
@@ -0,0 +1,19 @@
+﻿using Nodify.Interactivity;
+using System.Windows.Input;
+
+namespace Automation.App.Views.WorkPages.Workflows.Editor.Gestures
+{
+    public class LockedGestureMappings : EditorGestures
+    {
+        public static readonly LockedGestureMappings Instance = new LockedGestureMappings();
+
+        public LockedGestureMappings()
+        {
+            Editor.Selection.Apply(SelectionGestures.None);
+            ItemContainer.Selection.Apply(SelectionGestures.None);
+            Connection.Disconnect.Value = MultiGesture.None;
+            Connector.Connect.Value = MultiGesture.None;
+            Editor.Pan.Value = new AnyGesture(new System.Windows.Input.MouseGesture(MouseAction.MiddleClick));
+        }
+    }
+}
