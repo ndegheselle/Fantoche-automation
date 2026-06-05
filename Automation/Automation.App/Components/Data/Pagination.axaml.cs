72c2b4d1bc12f6a5381e065f7135ea3cc05512a8
Nicolas de gheselle <nicolas@degheselle.fr>
2026-06-05 23:35:19 +0200

    Add base visual for package page

---
+++ Automation/Automation.App/Components/Data/Pagination.axaml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Automation.App.Components.Data;
+
+public partial class Pagination : UserControl
+{
+    public Pagination()
+    {
+        InitializeComponent();
+    }
+}
