ce42b68ebcdf3a93cc2f6ac9610f02e20fdad782
Nicolas de gheselle <nicolas@degheselle.fr>
2026-08-19 20:18:15 +0200

    Add real binding on packages pag"e

--- Automation/Automation.App/Features/Packages/PackagesPage.xaml.cs
+++ Automation/Automation.App/Features/Packages/PackagesPage.xaml.cs
@@ -1,3 +1,7 @@
+using Automation.Shared.Base;
+using Microsoft.Win32;
+using System.IO;
+using System.Linq.Expressions;
 using System.Windows.Controls;
 
 namespace Automation.App.Features.Packages
@@ -7,9 +11,40 @@ namespace Automation.App.Features.Packages
     /// </summary>
     public partial class PackagesPage : UserControl
     {
+        public PackagesViewModel ViewModel => (PackagesViewModel)this.DataContext;
+
+        private string _search = "";
+        private PaginationOptions _paginationOptions = new PaginationOptions();
+
         public PackagesPage()
         {
             InitializeComponent();
         }
+
+        #region UI events
+        private void Search_SearchChanged(string search)
+        {
+            _search = search;
+            ViewModel.Search(_search, _paginationOptions);
+        }
+
+        private void Paging_PagingChange(int pageNumber, int capacity)
+        {
+            _paginationOptions = new PaginationOptions() { Page = pageNumber, PageSize = capacity };
+            ViewModel.Search(_search, _paginationOptions);
+        }
+
+        private void SelectFile(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Packages files (*.nupkg)|*.nupkg"
+            };
+            if (openFileDialog.ShowDialog() != true)
+                return;
+
+            ViewModel.AddPackage(openFileDialog.FileName);
+        }
+        #endregion
     }
 }
--- Automation/Automation.App/Features/Packages/PackagesPage.xaml.vm.cs
+++ Automation/Automation.App/Features/Packages/PackagesPage.xaml.vm.cs
@@ -1,12 +1,18 @@
+using Automation.Shared.Base;
+using Automation.Shared.Data.Execution;
 using Automation.Shared.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Joufflu.Inputs.Controls;
 using Joufflu.Navigation;
-using Joufflu.Navigation.Controls;
 
 namespace Automation.App.Features.Packages
 {
     public partial class PackagesViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private Paginated<PackageInfos> result = new Paginated<PackageInfos>();
+
         private readonly IPackagesService packages;
         private readonly IOverlayService overlays;
 
@@ -16,6 +22,17 @@ namespace Automation.App.Features.Packages
             this.overlays = overlays;
         }
 
+        public async void Search(string search, PaginationOptions options)
+        {
+            Result = await packages.SearchAsync(search, options);
+        }
+
+        public async void AddPackage(string filePaths)
+        {
+            await packages.AddAsync(filePaths);
+        }
+
+        [RelayCommand]
         public void OpenPackage()
         {
             overlays.Show(new PackageViewModel(), new OverlayOptions() { FullScreen = true, Title = "Package detail" });
